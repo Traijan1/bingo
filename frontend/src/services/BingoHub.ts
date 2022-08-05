6c0b62376f5a7d5acef2b9e133bfb7b8bbaf9acc
= <=>
2022-08-05 12:40:49 +0200

    Tested

--- frontend/src/services/BingoHub.ts
+++ frontend/src/services/BingoHub.ts
@@ -1,7 +1,7 @@
 import { HubConnectionBuilder } from "@microsoft/signalr";
 import url from "@/assets/domain.json";
 
-const domain = `${url}/bingoHub`;
+const domain = `${url.url}/bingoHub`;
 
 const BingoHub = new HubConnectionBuilder().withUrl(domain).build();
 
--- frontend/src/services/BingoService.ts
+++ frontend/src/services/BingoService.ts
@@ -1,9 +1,9 @@
 import { Board } from "@/models/board";
-import url from "@/assets/domain.json";
+import domain from "@/assets/domain.json";
 
 class BingoService {
     public async getBoard(id: string): Promise<Board> {
-        const response = await fetch(`${url}/board?id=${id}`);
+        const response = await fetch(`${domain.url}/board?id=${id}`);
         const json = await response.json();
 
         return json as Board;
